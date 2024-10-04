3be876b09c498f6c7fed9eb835f65aa86748a085
Folengui <hugo.fole.abellas@udc.es>
2024-10-04 09:52:59 +0200

    HOLA MO

--- 48111493R_48116978N_35632964F_48119893Y.jl
+++ 48111493R_48116978N_35632964F_48119893Y.jl
@@ -341,12 +341,12 @@ function trainClassCascadeANN(maxNumNeurons::Int,
     transferFunction::Function=σ,
     maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.001, minLossChange::Real=1e-7, lossChangeWindowSize::Int=5)
      
-    inputs = permutedims(trainingDataset[1])
+   
+    targets = permutedims(trainingDataset[2])
+
     
 
-    inputs = convert(Array{Float32},inputs)
-
-    trainingDataset = (inputs,trainingDataset[2])
+    trainingDataset = (trainingDataset[1],targets)
 
     #Crear RNA sin capas ocultas + entrenarla
     num_inputs = size(trainingDataset[1],2)
